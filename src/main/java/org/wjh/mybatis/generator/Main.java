9a0580c947212a71d1ac77dca60a0183bcb96b79
wangjihui <wangjihui@debian>
2017-09-02 23:18:28 +0800

    mod:update

--- src/main/java/org/wjh/mybatis/generator/Main.java
+++ src/main/java/org/wjh/mybatis/generator/Main.java
@@ -8,6 +8,6 @@ public class Main {
     public static void main(String[] args) throws Throwable {
         final String targetSourceDir = "C:\\Users";
 
-        new FileGenerator(targetSourceDir, "module_content", "com.netease.open.classbreak.domain.ModuleContent", true).generateFile();
+        new FileGenerator(targetSourceDir, "user", "org.wjh.domain.User", true).generateFile();
     }
 }
